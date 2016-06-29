1ac1093682fb7f091c0a3ae7f49caf2b5b49e6ef
nickwn <nick.thenebels@gmail.com>
2016-06-29 10:43:18 -0700

    new homepage

---
+++ js/text.js
@@ -0,0 +1,46 @@
+var text =
+{
+	notRight: "Oops, that's not a valid command... try '?'\n",
+	nick: "I'm a high school student living in San Diego, California\n" +
+				"I currently participate in Robotics, where I teach classes on Computer Vision, Arduino, and the Raspberry Pi\n" +
+				"I also participate in Cyber Security, where I sign in every meeting and do nothing else\n" +
+				"See my cool github accout <a href='http://github.com/nickwn'>here</a>\n",
+	ls: "<span class='jqconsole-ansi-color-blue'>launch_codes</span>\t" +
+			"<span class='jqconsole-ansi-color-green'>get_free_wifi</span>\t\n",
+	help: "nick - a little bit about me\n" +
+				"projects - a list of all my projects with links and descriptions\n" +
+				"contact - my contact info\n" +
+				"alt - go to my alternate home page\n" +
+				"help - :)\n",
+	projects: "<a href='http://github.com/nickwn/game-engine'>game-engine</a> - (not a game engine) A 3d renderer written in Java using only the standard libraries\n" +
+						"<a href='http://github.com/nickwn/ProjectATOD'>Project ATOD</a> - An APCS Final Project written in Java using the game engine libgdx\n" +
+						"<a href='http://github.com/nickwn/bluebot'>bluebot</a> - A Slack-integrated hubot that drastically increases and decreases Option 16's productivity\n" +
+						"<a href='http://github.com/nickwn/cv-rectangle-detector'>Rectangle Detector</a> - A small program that can detect a rectangle in a given image and outline it\n" +
+						"<a href='http://github.com/nickwn/ngine'>Ngine</a> - Coming soon\n",
+	contact: "email: nick.thenebels@gmail.com\ncontact me <a href='mailto:nick.thenebels@gmail.com'>here</a>\n",
+
+	alt: "directing to alternate home page...",
+
+	wifiStream:
+	[
+		"starting wifi autoconfig\n",
+		"searching for wifi\n",
+		"found no open networks\n",
+		"found secure net SSID 'Lenhart Family'\n",
+		"trying common passwords... ",
+		"failed\n",
+		"checking for WEP vulnerabilities... ",
+		"none found \n",
+		"connecting to bluetooth phone...\n",
+		"calling local school...",
+		"found Lenhart children\n",
+		"notifying field agents...",
+		"children acquired\n",
+		"calling Lenhart parents...\n",
+		"Negotiating for wifi password...\n",
+		"Conected to secure net SSID 'Lenhart Family'\n",
+		"source: <a href='https://xkcd.com/416/'>here</a>\n"
+	],
+
+	launchCodes: "Access Denied (nice try)\n"
+}
