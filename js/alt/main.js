4ca4371b3d4eecc89d788a33d49db12cc21039f1
nickwn <nick.thenebels@gmail.com>
2017-03-19 15:31:09 -0700

    new website design

---
+++ js/alt/main.js
@@ -0,0 +1,60 @@
+$(function ()
+{
+  var jqconsole = $('#console').jqconsole('Hi, I\'m Nick\nEnter \'nick\' to get started. Then try \'help\' for more\n', '~$ ');
+
+  function process(input)
+  {
+    switch(input)
+    {
+      case "./get_free_wifi":
+        // loops wont work for some reason
+        setTimeout(function(){jqconsole.Write(text.wifiStream[0], 'jqconsole-output', false);}, 1000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[1], 'jqconsole-output', false);}, 3000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[2], 'jqconsole-output', false);}, 5000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[3], 'jqconsole-output', false);}, 7000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[4], 'jqconsole-output', false);}, 9000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[5], 'jqconsole-output', false);}, 11000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[6], 'jqconsole-output', false);}, 13000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[7], 'jqconsole-output', false);}, 15000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[8], 'jqconsole-output', false);}, 17000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[9], 'jqconsole-output', false);}, 19000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[10], 'jqconsole-output', false);}, 21000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[11], 'jqconsole-output', false);}, 23000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[12], 'jqconsole-output', false);}, 25000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[13], 'jqconsole-output', false);}, 27000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[14], 'jqconsole-output', false);}, 29000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[15], 'jqconsole-output', false);}, 33000);
+        setTimeout(function(){jqconsole.Write(text.wifiStream[16], 'jqconsole-output', false);}, 33000);
+        return ""
+      case "cd launch_codes":
+        return text.launchCodes;
+      case "ls":
+        return text.ls;
+      case "help":
+        return text.help;
+      case "nick":
+        return text.nick;
+      case "projects":
+        return text.projects;
+      case "contact":
+        return text.contact;
+      case "alt":
+        self.location = "./alt.html";
+        return text.alt;
+      default:
+        return text.notRight;
+    }
+  }
+
+  var startPrompt = function ()
+  {
+    jqconsole.Prompt(true, function (input)
+    {
+      if(input) jqconsole.Write(process(input), 'jqconsole-output', false);
+      else jqconsole.Write(text.notRight, 'jqconsole-output');
+      startPrompt();
+    });
+  };
+
+  startPrompt();
+});
--- js/main.js
+++ js/main.js
@@ -1,60 +1,41 @@
-$(function ()
-{
-  var jqconsole = $('#console').jqconsole('Hi, I\'m Nick\nEnter \'nick\' to get started. Then try \'help\' for more\n', '~$ ');
-
-  function process(input)
-  {
-    switch(input)
-    {
-      case "./get_free_wifi":
-        // loops wont work for some reason
-        setTimeout(function(){jqconsole.Write(text.wifiStream[0], 'jqconsole-output', false);}, 1000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[1], 'jqconsole-output', false);}, 3000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[2], 'jqconsole-output', false);}, 5000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[3], 'jqconsole-output', false);}, 7000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[4], 'jqconsole-output', false);}, 9000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[5], 'jqconsole-output', false);}, 11000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[6], 'jqconsole-output', false);}, 13000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[7], 'jqconsole-output', false);}, 15000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[8], 'jqconsole-output', false);}, 17000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[9], 'jqconsole-output', false);}, 19000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[10], 'jqconsole-output', false);}, 21000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[11], 'jqconsole-output', false);}, 23000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[12], 'jqconsole-output', false);}, 25000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[13], 'jqconsole-output', false);}, 27000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[14], 'jqconsole-output', false);}, 29000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[15], 'jqconsole-output', false);}, 33000);
-        setTimeout(function(){jqconsole.Write(text.wifiStream[16], 'jqconsole-output', false);}, 33000);
-        return ""
-      case "cd launch_codes":
-        return text.launchCodes;
-      case "ls":
-        return text.ls;
-      case "help":
-        return text.help;
-      case "nick":
-        return text.nick;
-      case "projects":
-        return text.projects;
-      case "contact":
-        return text.contact;
-      case "alt":
-        self.location = "./alt.html";
-        return text.alt;
-      default:
-        return text.notRight;
-    }
+function handleTerm(event) {
+  var input = document.getElementsByName('term')[0].value;
+  console.log(input);
+  switch(input) {
+    case 'about':
+      smoothScroll.animateScroll(document.querySelector( '#about' ));
+      document.location = './index.html#about';
+      break;
+    case 'projects':
+      smoothScroll.animateScroll(document.querySelector( '#projects' ));
+      document.location = './index.html#projects';
+      break;
+    case 'alt':
+      document.location = "./alt.html";
+      break;
+    case 'altalt':
+      document.location = "./altalt.html";
+      break;
   }
+}
 
-  var startPrompt = function ()
-  {
-    jqconsole.Prompt(true, function (input)
-    {
-      if(input) jqconsole.Write(process(input), 'jqconsole-output', false);
-      else jqconsole.Write(text.notRight, 'jqconsole-output');
-      startPrompt();
-    });
-  };
+function currentYPosition() {
+    // Firefox, Chrome, Opera, Safari
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+}
 
-  startPrompt();
-});
+function elmYPosition(eID) {
+    var elm = document.getElementById(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+    } return y;
+}
